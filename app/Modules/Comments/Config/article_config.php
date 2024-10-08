d5a76148153e074c15266605bf18862c6b5c16a6
nicolas <158532428+Nicolas2-dev@users.noreply.github.com>
2024-10-08 17:20:29 +0200

    update config fmanager

--- app/Modules/Comments/Config/article_config.php
+++ app/Modules/Comments/Config/article_config.php
@@ -7,45 +7,66 @@ use Npds\Support\Facades\DB;
  */
 return [
 
-    // racine du nom de ce fichier  (article, pollBoth, ...)
+    /**
+     * racine du nom de ce fichier  (article, pollBoth, ...)
+     */
     'file_name' => 'article',
-
-    // permet d'allouer un numéro de forum pour chaque 'type de commentaires' (article, sondage, ...) - le numéro de forum doit impérativement être NEGATIF
+  
+    /**
+     * permet d'allouer un numéro de forum pour chaque 'type de commentaires'
+     * (article, sondage, ...) - le numéro de forum doit impérativement être NEGATIF
+     */
     'forum' => -1,
-
-    // permet d'allouer un numéro UNIQUE pour chaque publication sur laquelle un commentaire peut être réalisé (article numéro X, sondage numéro Y, ...)
+  
+    /**
+     * permet d'allouer un numéro UNIQUE pour chaque publication sur laquelle
+     * un commentaire peut être réalisé (article numéro X, sondage numéro Y, ...)
+     */
     'topic' => function ($sid) {
         return (isset($sid) ?: null);
     },
 
-    // URL de retour lorsque la soumission du commentaire est OK
+    /**
+     * URL de retour lorsque la soumission du commentaire est OK
+     */
     'url_ret' => function ($topic, $archive) {
-        return site_url('article.php?sid='. $topic .'&archive='. $archive);
+        return site_url('article?sid='. $topic .'&archive='. $archive);
     }, 
 
-    // Formulaire SFORM si vous souhaitez avoir une grille de saisie en lieu et place de l'interface standard de saisie - sinon ""
+    /**
+     * Formulaire SFORM si vous souhaitez avoir une grille de saisie 
+     * en lieu et place de l'interface standard de saisie - sinon ''
+     */
     'formulaire' => '',
-
-    // Nombre de commentaire sur chaque page
+ 
+    /**
+     * Nombre de commentaire sur chaque page
+     */
     'comments_per_page' => 2,
 
     // Mise à jour de champ d'une table externe à la table des commentaires
-
-    // opération à effectuer lorsque je rajoute un commentaire
+ 
+    /**
+     * opération à effectuer lorsque je rajoute un commentaire
+     */
     'comments_req_add' => function ($topic) {
         return DB::table('stories')->where('sid', $topic)->update([
             'comments' => DB::raw('comments+1')
         ]);
     },
 
-    // opération à effectuer lorsque je cache un commentaire
+    /**
+     * opération à effectuer lorsque je cache un commentaire
+     */
     'comments_req_del' => function ($topic) {
         return DB::table('stories')->where('sid', $topic)->update([
             'comments' => DB::raw('comments-1')
         ]);
     },
 
-    // opération à effectuer lorsque je supprime tous les commentaires
+    /**
+     * opération à effectuer lorsque je supprime tous les commentaires
+     */
     'comments_req_raz' => function ($topic) {
         return DB::table('stories')->where('sid', $topic)->update([
             'comments' => 0
--- app/Modules/Links/Config/config.php
+++ app/Modules/Links/Config/config.php
@@ -5,6 +5,42 @@
  */
 return [
 
+    /**
+     * The Mysql-table prefix for all THIS instance of the module
+     */
+    'links_DB' => '',
 
+    /**
+     * Allow to register http links (yes=>true, no=>false, both=>false-true => -1)
+     */
+    'links_url' => false - true,
+ 
+    /**
+     * Allow to register links with the Topic's information (true) or not (false)
+     */
+    'links_topic' => true,
+ 
+    /**
+     * Allow to limit the number of subcat showed in main page
+     * ("limit 0,0" => no subcat / "limit 0,3" => 3 subcat ... / "" => no limit )
+     * 
+     * You can also do a more complexe query :
+     * for exemple : $subcat_limit="ASC limit 0,5" because the $subcat_limit complete the sql query !
+     */
+    'subcat_limit' => '',
+
+    /**
+     * From Npds config : Is Anonymous autorise to post new links? (0=Yes 1=No)
+     * 
+     * You can affect other value : App authorisations :
+     *      -127 : admin only
+     *      -1 : anonymous only
+     *      0 : All (like Npds config)
+     *      1 : Member only (Npds config)
+     *      2 to 126 : group' members ("gp1,gp2,gp3" is also possible)
+     * 
+     * exepmle : links_anonaddlinklock=-127;
+     */
+    'links_anonaddlinklock' => '',
 
 ];
