aa24f4b201df79c72c2cad68f9cf9c914d12041e
nicolas <158532428+Nicolas2-dev@users.noreply.github.com>
2024-10-15 16:26:14 +0200

    update module news NewKernel class

--- Modules/News/Bootstrap/NewsKernel.php
+++ Modules/News/Bootstrap/NewsKernel.php
@@ -11,6 +11,11 @@ class NewsKernel
 {
 
     /**
+     * 
+     */
+    protected static $module_path;
+
+    /**
      * Undocumented variable
      *
      * @var array
@@ -25,6 +30,36 @@ class NewsKernel
     ];
 
     /**
+     * [$instance description]
+     *
+     * @var [type]
+     */
+    protected static $instance;
+
+
+    /**
+     * Undocumented function
+     */
+    public function __construct($directory)
+    {
+        static::$module_path = dirname(dirname($directory)) . DS;
+    }
+
+    /**
+     * [getInstance description]
+     *
+     * @return  [type]  [return description]
+     */
+    public static function getInstance($directory)
+    {
+        if (isset(static::$instance)) {
+            return static::$instance;
+        }
+
+        return static::$instance = new static($directory);
+    }
+
+    /**
      * Undocumented function
      *
      * @return void
@@ -32,6 +67,80 @@ class NewsKernel
     public static function aliases_loader()
     {
         AliasLoader::getInstance(static::$aliases)->register();
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_constant()
+    {
+        include static::$module_path .'constants.php';
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_helper()
+    {
+        include static::$module_path .'Support'. DS .'helpers.php';
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_boxe()
+    {
+        include static::$module_path .'Boxe'. DS .'Boxe.php';
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_route_web()
+    {
+        include static::$module_path .'Routes'. DS .'web'. DS .'routes.php';
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_route_admin()
+    {
+        include static::$module_path .'Routes'. DS .'admin'. DS .'routes.php';
+
+        return static::$instance;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function load_route_api()
+    {
+        // include static::$module_path .'Routes'. DS .'api'. DS .'routes.php';
+
+        // return static::$instance;
     }
 
 }
